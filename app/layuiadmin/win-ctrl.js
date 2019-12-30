114b7980a1319c2100321f17dcf421feadd5a34b
nwdxlgzs <47502008+nwdxlgzs@users.noreply.github.com>
2019-12-30 12:56:19 +0800

    Add files via upload

---
+++ app/layuiadmin/win-ctrl.js
@@ -0,0 +1,39 @@
+﻿     // require('electron').ipcRenderer;
+      // You can also require other files to run in this process
+
+      require('../renderer.js')
+
+var ipc = require('electron').ipcRenderer;
+document.getElementById('maxbt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-max');
+ 
+})
+document.getElementById('minbt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-min');
+ 
+})
+document.getElementById('closebt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-close');
+ 
+})
+
+
+var ipc = require('electron').ipcRenderer;
+document.getElementById('maxbt1').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-max');
+ 
+})
+document.getElementById('minbt1').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-min');
+ 
+})
+document.getElementById('closebt1').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-close');
+ 
+})
