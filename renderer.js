19c90b5755aa5f1dbc1a163a0a84843d3fc20fa7
nwdxlgzs <47502008+nwdxlgzs@users.noreply.github.com>
2019-12-30 13:05:32 +0800

    Add files via upload

---
+++ renderer.js
@@ -0,0 +1,20 @@
+﻿// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+var ipc = require('electron').ipcRenderer;
+document.getElementById('maxbt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-max');
+ 
+})
+document.getElementById('minbt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-min');
+ 
+})
+document.getElementById('closebt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-close');
+ 
+})
