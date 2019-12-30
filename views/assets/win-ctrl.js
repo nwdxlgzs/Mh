ff265d6d188933c87c20040bfbd56ee37b393fea
nwdxlgzs <47502008+nwdxlgzs@users.noreply.github.com>
2019-12-30 13:14:02 +0800

    Add files via upload

---
+++ views/assets/win-ctrl.js
@@ -0,0 +1,21 @@
+﻿     // require('electron').ipcRenderer;
+      // You can also require other files to run in this process
+
+      require('../renderer.js')
+
+var ipc = require('electron').ipcRenderer;
+document.getElementById('maxbt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-max');
+ 
+})
+document.getElementById('minbt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-min');
+ 
+})
+document.getElementById('closebt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-close');
+ 
+})
